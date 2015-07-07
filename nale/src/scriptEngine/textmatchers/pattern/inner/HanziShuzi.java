ccac7f8a33094589b547f01eb91f31d5a30f4fb5
mdxys <mdxys@163.com>
2015-07-07 15:44:00 +0800

    d

--- nale/src/scriptEngine/textmatchers/pattern/inner/HanziShuzi.java
+++ nale/src/scriptEngine/textmatchers/pattern/inner/HanziShuzi.java
@@ -1,4 +1,4 @@
-﻿package textmatchers.pattern.inner;
+package textmatchers.pattern.inner;
 
 import textmatchers.pattern.def.reg.REnum;
 
@@ -6,9 +6,7 @@ public class HanziShuzi extends REnum {
 
 	public HanziShuzi(int min, int max) {
 		super(min, max, new char[] { '一', '二', '三', '四', '五', '六', '七', '八',
-				'九', '十',
-
-		});
+				'九', '十', });
 
 	}
 
